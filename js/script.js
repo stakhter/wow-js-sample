e760b6db5c9b22930e4082f471b09bc33f9ee155
St Akhter(Tanha) <128251318+stakhter@users.noreply.github.com>
2023-08-06 04:00:34 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//js code
+
+wow = new WOW(
+{
+  	boxClass:     'wow',     
+ 	animateClass: 'animated', 
+  	offset:       0,          
+  	mobile:       true,      
+  	live:         true        
+})
+wow.init();
+
+
+
+
+//jq code
+$(document).ready(function(){
+
+
+	
+})
